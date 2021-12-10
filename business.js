8343facc064d925a4f772a1f6abdd57621681d71
Kirill Borovikov <bks1105@gmail.com>
2021-12-10 23:55:41 +0300

    Work with modules

--- business.js
+++ business.js
@@ -1,3 +1,126 @@
+let RESPONSE = new Object();
+
+function genData(recCount){
+    let arr = new Array();
+    for(i = 0; i <= recCount; i++){
+        b = i % 2 === 0 ? true : false
+        let d = new Date();
+        let dStr = d.getUTCFullYear() +"/"+ 
+                    (d.getUTCMonth()+1) +"/"+ 
+                    d.getUTCDate() + " " + 
+                    d.getHours() + ":" + 
+                    d.getUTCMinutes() + ":" + 
+                    d.getUTCSeconds();
+        let itm = {
+            id : i,
+            title : "Titel" + i,
+            time : dStr,
+            active : b
+        }
+      arr[i] = itm;   
+    }
+    RESPONSE.data = arr;
+    return arr;
+}
+
+function wrapToJSON(){
+  // let dataVal = genData(7);
+  // let dataObj = {
+  //     data : dataVal
+  // };  
+  //let jsonString = JSON.stringify(RESPONSE);
+  //debug
+  //console.log(jsonString);
+  //console.log(dataVal[5].active);
+ // console.log(dataVal[4].active);
+  // end debug
+  let dataRef = "text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(RESPONSE));
+  let aref = document.createElement('a');
+  aref.href = 'data:' + dataRef;
+  aref.download = 'data.json';
+  aref.innerHTML = 'download JSON';
+
+  let container = document.getElementById('hrefcontainer');
+  container.appendChild(aref);
+}
+
+function getData(){
+  // let dataVal = genData(1005);
+  // let dataObj = {
+  //     data : dataVal
+  // };
+  document.getElementById("jsonShow").innerHTML = JSON.stringify(RESPONSE, undefined, 2);
+}
+
+function renderData(resp){
+  //   let da = genData(1100);
+  //   let dataObj = {
+  //   data : da
+  // };
+    let tblCont = document.getElementById("tblArea");
+    let tblData = resp.data;
+    //let ii = tblData.length;
+    //console.log(ii);
+
+    //Create table
+    let tbl = document.createElement('table');
+
+    let tblHeaderRow = document.createElement('tr');
+
+    let idHeader = document.createElement('th');
+    idHeader.appendChild(document.createTextNode("id"));
+    let titleHeader = document.createElement('th');
+    titleHeader.appendChild(document.createTextNode("title"));
+    let timeHeader = document.createElement('th');
+    timeHeader.appendChild(document.createTextNode("time"));
+    let activeHeader = document.createElement('th');
+    activeHeader.appendChild(document.createTextNode("active"));
+
+    tblHeaderRow.appendChild(idHeader);
+    tblHeaderRow.appendChild(titleHeader);
+    tblHeaderRow.appendChild(timeHeader);
+    tblHeaderRow.appendChild(activeHeader);
+    tbl.appendChild(tblHeaderRow);
+  
+    for(i = 0; i < tblData.length; i++){
+        let tblRow = document.createElement('tr');  
+
+        let idData = tblData[i].id;
+        let titleData = tblData[i].title;
+        let timeData = tblData[i].time;
+        let activeData = tblData[i].active;
+        
+        let idCell = document.createElement('td');
+        let titleCell = document.createElement('td');
+        let timeCell  = document.createElement('td');
+        let activeCell = document.createElement('td');
+
+        idCell.appendChild(document.createTextNode(idData));
+        titleCell.appendChild(document.createTextNode(titleData));
+        timeCell.appendChild(document.createTextNode(timeData));
+        activeCell.appendChild(document.createTextNode(activeData));
+
+        tblRow.appendChild(idCell);
+        tblRow.appendChild(titleCell);
+        tblRow.appendChild(timeCell);
+        tblRow.appendChild(activeCell);
+
+        tbl.appendChild(tblRow);
+    }
+    tblCont.appendChild(tbl);
+}
+
+function sendUpdate(resp){
+    let activeRecords = RESPONSE.data.filter(a => a.active == true);
+    console.log(activeRecords);
+}
+
+function doSomeSteps(someData){
+    showAlert(someData);
+}
+
+export { RESPONSE, genData, renderData };
+
 // function readJSONFromFile(callback){
 //     let xobj = new XMLHttpRequest();
 //     xobj.overrideMimeType("application/json");
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-//import * as bussinesModule from 'bussines.js'
+import { genData }   from '/business.js';
+
 const COLOR_RED = "#F00";
 const COLOR_GREEN = "#0F0";
 const COLOR_BLUE = "#00F";
 const COLOR_ORANGE = "#FF7F00";
+
 
 let lSideOpen = false;
 let rSideOpen = false;
@@ -17,13 +19,13 @@ window.onkeyup = function(event){
 function ShowLeftPanel(){
     alert("Left panel");
 }
-function openLeftNav() {
+export function openLeftNav() {
     document.getElementById("leftSidebar").style.width = "250px";
     lSideOpen = true;
   }
 
   function openRightNav() {
-    document.getElementById("rightSidebar").style.width = "250px";
+    document.getElementById("rightSidebar").style.width = "300px";
     rSideOpen = true;
   }
   
@@ -39,8 +41,10 @@ function closeRightNav() {
 function showModalWindow()
 {
     if(rSideOpen && lSideOpen){
-        let m = document.getElementById("mw");
-        m.style.display = "block";
+      
+      genData(1010);
+      let m = document.getElementById("mw");
+      m.style.display = "block";
     }
 }
 
@@ -58,115 +62,6 @@ function closeModalWindow(){
 // BUSSINES PART
 
 
-function genData(recCount){
-    let arr = new Array();
-    for(i = 0; i <= recCount; i++){
-        b = i % 2 === 0 ? true : false
-        let d = new Date();
-        let dStr = d.getUTCFullYear() +"/"+ 
-                    (d.getUTCMonth()+1) +"/"+ 
-                    d.getUTCDate() + " " + 
-                    d.getHours() + ":" + 
-                    d.getUTCMinutes() + ":" + 
-                    d.getUTCSeconds();
-        let itm = {
-            id : i,
-            title : "Titel" + i,
-            time : dStr,
-            active : b
-        }
-      arr[i] = itm;   
-    }
-    
-    return arr;
-}
-
-function wrapToJSON(){
-  let dataVal = genData(7);
-  let dataObj = {
-      data : dataVal
-  };  
-  let jsonString = JSON.stringify(dataObj);
-  //debug
-  console.log(jsonString);
-  console.log(dataVal[5].active);
-  console.log(dataVal[4].active);
-  // end debug
-  let dataRef = "text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(dataObj));
-  let aref = document.createElement('a');
-  aref.href = 'data:' + dataRef;
-  aref.download = 'data.json';
-  aref.innerHTML = 'download JSON';
-
-  let container = document.getElementById('hrefcontainer');
-  container.appendChild(aref);
-}
-
-function showFormatedJSON(){
-  let dataVal = genData(1005);
-  let dataObj = {
-      data : dataVal
-  };
-  document.getElementById("jsonShow").innerHTML = JSON.stringify(dataObj, undefined, 2);
-}
-
-function showTable(){
-    let da = genData(30);
-    let dataObj = {
-      data : da
-  };
-    let tblCont = document.getElementById("tblArea");
-    let tblData = dataObj.data;
-    //let ii = tblData.length;
-    //console.log(ii);
-
-    //Create table
-    let tbl = document.createElement('table');
-
-    let tblHeaderRow = document.createElement('tr');
-
-    let idHeader = document.createElement('th');
-    idHeader.appendChild(document.createTextNode("id"));
-    let titleHeader = document.createElement('th');
-    titleHeader.appendChild(document.createTextNode("title"));
-    let timeHeader = document.createElement('th');
-    timeHeader.appendChild(document.createTextNode("time"));
-    let activeHeader = document.createElement('th');
-    activeHeader.appendChild(document.createTextNode("active"));
-
-    tblHeaderRow.appendChild(idHeader);
-    tblHeaderRow.appendChild(titleHeader);
-    tblHeaderRow.appendChild(timeHeader);
-    tblHeaderRow.appendChild(activeHeader);
-    tbl.appendChild(tblHeaderRow);
-  
-    for(i = 0; i < tblData.length; i++){
-        let tblRow = document.createElement('tr');  
-
-        let idData = tblData[i].id;
-        let titleData = tblData[i].title;
-        let timeData = tblData[i].time;
-        let activeData = tblData[i].active;
-        
-        let idCell = document.createElement('td');
-        let titleCell = document.createElement('td');
-        let timeCell  = document.createElement('td');
-        let activeCell = document.createElement('td');
-
-        idCell.appendChild(document.createTextNode(idData));
-        titleCell.appendChild(document.createTextNode(titleData));
-        timeCell.appendChild(document.createTextNode(timeData));
-        activeCell.appendChild(document.createTextNode(activeData));
-
-        tblRow.appendChild(idCell);
-        tblRow.appendChild(titleCell);
-        tblRow.appendChild(timeCell);
-        tblRow.appendChild(activeCell);
-
-        tbl.appendChild(tblRow);
-    }
-    tblCont.appendChild(tbl);
-}
 
 // function readJson(){
 //     let client = new XMLHttpRequest();
