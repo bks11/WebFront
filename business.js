7bf57150a81d3b5586b9372dfde48cd337125baf
Kirill Borovikov <bks1105@gmail.com>
2021-12-15 23:10:09 +0300

    Work with array move DOM action to index.js

--- business.js
+++ business.js
@@ -1,199 +1,149 @@
-let RESPONSE = new Object();
+let RESPONSE = new Object()
 
-function genData(recCount){
-    let arr = new Array();
-    for(let i = 0; i <= recCount; i++){
-        const b = i % 2 === 0 
-        let d = new Date();
-        let dStr = d.getUTCFullYear() +"/"+ 
-                    (d.getUTCMonth()+1) +"/"+ 
-                    d.getUTCDate() + " " + 
-                    d.getHours() + ":" + 
-                    d.getUTCMinutes() + ":" + 
-                    d.getUTCSeconds();
-        let itm = {
+// to do {}  Array methods!!! reduce map 
+// All dom  action move to index.js
+// Change function  to => ,  remove  
+// Add method to load data from file
+// Read about back qoutes 
+
+
+const genData = (recCount) => {
+  let arr = new Array()
+  for(let i = 0; i <= recCount; i++) {
+    const b = i % 2 === 0 
+    let d = new Date()
+    let dStr = d.getUTCFullYear() +'/'+ 
+                (d.getUTCMonth()+1) +'/'+ 
+                 d.getUTCDate() + ' ' + 
+                 d.getHours() + ':' + 
+                 d.getUTCMinutes() + ':' + 
+                 d.getUTCSeconds()
+    let itm = {
             id : i,
-            title : "Titel" + i,
+            title : 'Titel' + i,
             time : dStr,
             active : b
-        }
-      arr[i] = itm;   
     }
-    RESPONSE.data = arr;
-    return arr;
+    arr[i] = itm   
+	}
+  RESPONSE.data = arr
+  return arr
+}
+  // move work with DOM elements to index.js 
+	// only data calculation
+
+
+
+const sendUpdate = () => {
+  const activeRecords = RESPONSE.data.filter(a => a.active)
+  console.log(activeRecords)
 }
 
-function wrapToJSON(){
-  // let dataVal = genData(7);
-  // let dataObj = {
-  //     data : dataVal
-  // };  
-  //let jsonString = JSON.stringify(RESPONSE);
-  //debug
-  //console.log(jsonString);
-  //console.log(dataVal[5].active);
- // console.log(dataVal[4].active);
-  // end debug
-  let dataRef = "text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(RESPONSE));
-  let aref = document.createElement('a');  // const
-  aref.href = 'data:' + dataRef;
-  aref.download = 'data.json';
-  aref.innerHTML = 'download JSON';
+const doSomeSteps = (someData) => {
+  someData = RESPONSE.data
+	console.log(someData[0])
+	console.log(someData.length)
+	//Add last row copy to the end of array
+	const lastRow = someData.at(-1) // get last row value
+	console.log(lastRow) //Show last row value
+  console.log(`Array length before push ${someData.length}`)
+	const newLength = someData.push(lastRow) //Add to end of array  new element
+  console.log(`New length someData array ${newLength}`)
+	
+	//Clone object someData someDataClone
+	const someDateClone = JSON.parse(JSON.stringify(someData))
+	//Change title for the first row
+	someData[0].title = 'Title01'
+  console.log(`Clone some data ${someDateClone[0].title}`)
+  console.log(`Some data ${someData[0].title}`)
 
-  let container = document.getElementById('hrefcontainer');
-  container.appendChild(aref);
+	//Add range from 0  to 5  -   slice splice  clone type ?  Мутирующие операции в методах массива
+	let partData = someData.slice(0, 5)
+  console.log(partData)
+  let testMap = partData.map( (x) => {return `<tr><td>${x.id}</td><td>${x.title}</td><td>${x.time}</td><td>${x.active}</td></tr>`})
+  console.log(testMap)
+
+
+	
 }
+    
 
-function getData(){
-  // let dataVal = genData(1005);
-  // let dataObj = {
-  //     data : dataVal
-  // };
-  document.getElementById("jsonShow").innerHTML = JSON.stringify(RESPONSE, undefined, 2);
-}
-const renderDataToTable = () => {
-    const tblData = RESPONSE.data;
-    if('content' in document.createElement('template')) {
-        const tbody = document.querySelector('tbody')
-        const template = document.querySelector('#recordrow')
+//Back quotes
+//const template = document.createElement('template')
+//const newrow = template.content.cloneNode(true)
 
-        for(let i = 0; i < tblData.length; i++) {
-            const newrow = template.content.cloneNode(true)
-            const td = newrow.querySelectorAll('td')
-            td[0].textContent = tblData[i].id
-            td[1].textContent = tblData[i].title
-            td[2].textContent = tblData[i].time
-            td[3].textContent = tblData[i].active
 
-            tbody.appendChild(newrow)
-        }
-    } else {
-        alert('Broser not supports template')
-    }
-}
-function renderData(){
-  //   let da = genData(1100);
-  //   let dataObj = {
-  //   data : da
-  // };
-    let tblCont = document.getElementById("tblArea");
-    let tblData = RESPONSE.data;
-    //let ii = tblData.length;
-    //console.log(ii);
+// const table = `<table>
+//   <thead>
+// 		<tr>
+// 				<td>id</td>
+// 				<td>title</td>
+// 				<td>time</td>
+// 				<td>active</td>
+// 		</tr>
+// </thead>
+// <tbody>
+// 		${}  // reduce map
+// </tbody>
+// </table>`
 
-    //read about  Template  
-    //document.createElement('template')
-        
-    //Create table
-    let tbl = document.createElement('table');
+// template.innerHTML = table
+// container.append(template.content.cloneNode(true))
 
-    let tblHeaderRow = document.createElement('tr');
 
-    let idHeader = document.createElement('th');
-    idHeader.appendChild(document.createTextNode("id"));
-    let titleHeader = document.createElement('th');
-    titleHeader.appendChild(document.createTextNode("title"));
-    let timeHeader = document.createElement('th');
-    timeHeader.appendChild(document.createTextNode("time"));
-    let activeHeader = document.createElement('th');
-    activeHeader.appendChild(document.createTextNode("active"));
-
-    tblHeaderRow.appendChild(idHeader);
-    tblHeaderRow.appendChild(titleHeader);
-    tblHeaderRow.appendChild(timeHeader);
-    tblHeaderRow.appendChild(activeHeader);
-    tbl.appendChild(tblHeaderRow);
-  
-    for(let i = 0; i < tblData.length; i++) {
-        let tblRow = document.createElement('tr');  
-
-        let idData = tblData[i].id;
-        let titleData = tblData[i].title;
-        let timeData = tblData[i].time;
-        let activeData = tblData[i].active;
-        
-        let idCell = document.createElement('td');
-        let titleCell = document.createElement('td');
-        let timeCell  = document.createElement('td');
-        let activeCell = document.createElement('td');
-
-        idCell.appendChild(document.createTextNode(idData));
-        titleCell.appendChild(document.createTextNode(titleData));
-        timeCell.appendChild(document.createTextNode(timeData));
-        activeCell.appendChild(document.createTextNode(activeData));
-
-        tblRow.appendChild(idCell);
-        tblRow.appendChild(titleCell);
-        tblRow.appendChild(timeCell);
-        tblRow.appendChild(activeCell);
-
-        tbl.appendChild(tblRow);
-    }
-    tblCont.appendChild(tbl);
-}
-
-function sendUpdate(){
-    let activeRecords = RESPONSE.data.filter(a => a.active);
-    console.log(activeRecords);
-}
-
-function doSomeSteps(){
-    showAlert(someData);
-}
-
-export { RESPONSE, getData, genData, renderData, sendUpdate, renderDataToTable };
+export {  genData,  sendUpdate,  doSomeSteps }
 
 // function readJSONFromFile(callback){
-//     let xobj = new XMLHttpRequest();
-//     xobj.overrideMimeType("application/json");
-//     xobj.open('GET','data.json',true);
+//     let xobj = new XMLHttpRequest()
+//     xobj.overrideMimeType('application/json')
+//     xobj.open('GET','data.json',true)
 //     xobj.onreadystatechange = function () {
-//         if (xobj.readyState == 4 && xobj.status == "200") {
+//         if (xobj.readyState == 4 && xobj.status == '200') {
 
 //             // .open will NOT return a value but simply returns undefined in async mode so use a callback
-// //             callback(xobj.responseText);
+// //             callback(xobj.responseText)
 // //         }
 // //     }
-// //     xobj.send(null);
+// //     xobj.send(null)
 // // }
 
 
 // // function getData(){
 // //     loadJSON(function(response) {
 // //         // Do Something with the response e.g.
-// //         let jsonresponse = JSON.parse(response);
+// //         let jsonresponse = JSON.parse(response)
         
 // //         // Assuming json data is wrapped in square brackets as Drew suggests
-// //         console.log(jsonresponse[0].name);
+// //         console.log(jsonresponse[0].name)
         
 // //         }
-// //     );
+// //     )
 // // }
 
 // function genereateObjects(){
-//     let arr = new Array();
-//     let b = i % 2 === 0 ? true : false;
+//     let arr = new Array()
+//     let b = i % 2 === 0 ? true : false
 
-//     for(i = 0; i <=1000; i++){
+//     for(i = 0 i <=1000 i++){
 //         let data = {
 //             id = i,
-//             title = "Titel" + i,
+//             title = 'Titel' + i,
 //             time = new Date,
 //             active =b
 //         }
-//         arr[i] = data;
+//         arr[i] = data
 //     }
-//     return arr;
+//     return arr
 // }
 
 // function generateJson(){
-//     let arr = genereateObjects();
-//     let jsonString = JSON.stringify(arr);
+//     let arr = genereateObjects()
+//     let jsonString = JSON.stringify(arr)
 // }
 
 // let data = {
 //     id = 0,
-//     title = "Title3",
+//     title = 'Title3',
 //     time = new Date().toUTCString,
 //     active = true
 // }
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 
-import { genData, RESPONSE, getData, renderData, sendUpdate, renderDataToTable } from './business.js'
+import { genData,  sendUpdate, doSomeSteps } from './business.js'
 // document.getElementById('btn_first').onclick = window.ShowLeftPanel Question?
-
 
 window.onkeyup = event => {
   if (event.keyCode == 27) {
@@ -12,67 +11,162 @@ window.onkeyup = event => {
 let lSideOpen = false
 let rSideOpen = false
 
-//let el = document.getElementById('rightSidebar')
-
-
 const openLeftNav = () => {
   document.getElementById('leftSidebar').style.width = '250px'
   lSideOpen = true
 }
 
-function closeLeftNav() {
+const closeLeftNav = () => {
   document.getElementById('leftSidebar').style.width = '0'
   lSideOpen = false
 }  
 
-function openRightNav() {
+const openRightNav = () => {
   document.getElementById('rightSidebar').style.width = '300px'
   rSideOpen = true
 }
   
 
 
-function closeRightNav() {
+const closeRightNav = () => {
   document.getElementById('rightSidebar').style.width = '0'
   rSideOpen = false
 }
-function showModalWindow()
-{
+const showModalWindow = () => {
   if(rSideOpen && lSideOpen) {
     genData(1010)
-    let m = document.getElementById('mw')
+    const m = document.getElementById('mw')
     m.style.display = 'block'
   }
 }
 
-function closeModalWindow(){
-  let m = document.getElementById('mw')
-   m.style.display = 'none'
+const closeModalWindow = () => {
+  const m = document.getElementById('mw')
+  m.style.display = 'none'
 }
 
+const wrapToJSON = () => {
+  let dataForJSONFile = new Object()
+  dataForJSONFile.data = genData(1010)
+  let dataRef = 'text/jsoncharset=utf-8,' + encodeURIComponent(JSON.stringify(dataForJSONFile))
+  const aref = document.createElement('a')  // const
+  aref.href = 'data:' + dataRef
+  aref.download = 'data.json'
+  aref.innerHTML = 'download JSON'
+
+  const container = document.getElementById('hrefcontainer')
+  container.appendChild(aref)
+}
+
+const getData = () => {
+  let dataJSON = new Object()
+  dataJSON.data = genData(1010)
+  document.getElementById('jsonShow').innerHTML = JSON.stringify(dataJSON, undefined, 2)
+}
+
+
+const renderDataToTable = () => {
+  let dataJSON = new Object()
+  dataJSON.data = genData(1010)
+  const tblData = dataJSON.data
+  if('content' in document.createElement('template')) {
+  	const tbody = document.querySelector('tbody')
+    const template = document.querySelector('#recordrow')
+
+    for(let i = 0; i < tblData.length; i++) {
+      const newrow = template.content.cloneNode(true)
+      const td = newrow.querySelectorAll('td')
+      td[0].textContent = tblData[i].id
+      td[1].textContent = tblData[i].title
+      td[2].textContent = tblData[i].time
+      td[3].textContent = tblData[i].active
+
+      tbody.appendChild(newrow)
+    }
+    } else {
+        alert('Broser not supports template')
+    }
+}
 
 window.openLeftNav = openLeftNav
 window.closeLeftNav = closeLeftNav
 window.openRightNav = openRightNav
 window.showModalWindow = showModalWindow
 window.closeModalWindow = closeModalWindow
-window.genData = genData
+window.wrapToJSON = wrapToJSON
 window.getData = getData
-window.renderData = renderData
-window.sendUpdate = sendUpdate
 window.renderDataToTable = renderDataToTable
 
+//business module
+window.genData = genData
+//window.renderData = renderData
+window.sendUpdate = sendUpdate
+window.doSomeSteps = doSomeSteps
 
-// const RESP = {
-//   a: function(){
-//     alert()
-//   },
-//   b: () => {
+const RESP = {
+  a: function(){
+    alert()
+  },
+  b: () => {
 
-//   },
-//   c() { 
-//     this === RESP
-//     this.a
+  },
+  c() { 
+    this === RESP
+    this.a
 
+  }
+}
+
+
+
+// const renderData = () => {
+//   const tblCont = document.getElementById('tblArea')
+//   let tblData = RESPONSE.data
+//     //Create table
+//   const tbl = document.createElement('table')
+//   const tblHeaderRow = document.createElement('tr')
+//   const idHeader = document.createElement('th')
+//   idHeader.appendChild(document.createTextNode('id'))
+//   const titleHeader = document.createElement('th')
+//   titleHeader.appendChild(document.createTextNode('title'))
+//   const timeHeader = document.createElement('th')
+//   timeHeader.appendChild(document.createTextNode('time'))
+//   const activeHeader = document.createElement('th')
+//   activeHeader.appendChild(document.createTextNode('active'))
+
+//   tblHeaderRow.appendChild(idHeader)
+//   tblHeaderRow.appendChild(titleHeader)
+//   tblHeaderRow.appendChild(timeHeader)
+//   tblHeaderRow.appendChild(activeHeader)
+//   tbl.appendChild(tblHeaderRow)
+  
+//   for(let i = 0; i < tblData.length; i++) {
+//     let tblRow = document.createElement('tr')  
+
+//     let idData = tblData[i].id
+//     let titleData = tblData[i].title
+//     let timeData = tblData[i].time
+//     let activeData = tblData[i].active
+        
+//     const idCell = document.createElement('td')
+//     const titleCell = document.createElement('td')
+//     const timeCell  = document.createElement('td')
+//     const activeCell = document.createElement('td')
+
+//     idCell.appendChild(document.createTextNode(idData))
+//     titleCell.appendChild(document.createTextNode(titleData))
+//     timeCell.appendChild(document.createTextNode(timeData))
+//     activeCell.appendChild(document.createTextNode(activeData))
+
+//     tblRow.appendChild(idCell)
+//     tblRow.appendChild(titleCell)
+//     tblRow.appendChild(timeCell)
+//     tblRow.appendChild(activeCell)
+
+//     tbl.appendChild(tblRow)
 //   }
+//   tblCont.appendChild(tbl)
+// }
+// const ff = () => {
+//   let a = getData()
 // }
